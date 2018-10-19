5beaaf844f72967cc89fb5b3e3fea9b2bb8e435e
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2018-10-19 22:08:30 +0200

    "start to work with functions. try to get html and javascript to interact. try to add some kind of eventhandler so something can happen when you press the buttons"

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Globala variabler
 
-var wordList; // Lista med spelets alla ord
+var wordList = ["Sol", "Träd", "Bänk", "Stol", "Knivig", "Busig", "Polis", "Gris", "Vegan", "Bil", "Rally", "Fralla", "Spring", "Regn"]; // Lista med spelets alla ord
 var selectedWord; // Ett av orden valt av en slumpgenerator
 var letterBoxes; //Rutorna där bokstäverna ska stå
 var hangmanImg; //Bild som kommer vid fel svar
@@ -17,13 +17,34 @@ function init() {} // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+/*funktion som just nu ändrar bakgrundsfärg när man trycker på starta spel-knappen men som ska ändras till att starta spelet vid tryck*/
+function startTheGame() {
+    document.querySelector("body").style.backgroundColor = "red";        
+}
+
+startGameBtn = document.getElementById("startGameBtn");
+startGameBtn.addEventListener('click', startTheGame); 
 
 // Funktion som slumpar fram ett ord
- 
+
+ /*Slumpar ord ur min ordlista, skriver ut dessa i meddelande-div:en sålänge.....*/
+ for(var i=0;i<wordList.length;i++) {    
+    selectedWord = Math.floor(Math.random() * wordList.length);
+var randomWord = document.getElementById("message");
+     randomWord.textContent= wordList[selectedWord]; 
+} 
+
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
 
+
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
+/*kod som just nu gör att en alert dyker upp när man klickar på bokstavsknapparna*/
+document.getElementById("letterButtons").addEventListener("click",function(){
+    alert("Du har klickat på en knapp!");});
+
 
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
 
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+
+
