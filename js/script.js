6df1e07a1b19f8df06c4ce51a4bd5dc61d83afeb
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2018-11-02 17:51:10 +0100

    change color on css elements  to black to get more contrast. add function to start game when press on button. add function to read value of buttonclick. add function to print out listitems depending on length of word. Add words in array, Add generator to pick random word. Add conditional statements if choosen letter is in random word or not. If it is, print out letter in list, if its not, show one picture of hanged man in right order dependingon number of wrong guess.

--- js/script.js
+++ js/script.js
@@ -1,50 +1,139 @@
 // Globala variabler
+// Lista med spelets alla ord
+const wordList = ["Sol", "Träd", "Bänk", "Stol", "Knivig", "Busig", "Polis", "Gris", "Vegan", "Bil", "Rally", "Fralla", "Spring", "Regn"]; 
+// Ett av orden valt av en slumpgenerator
+let selectedWord =  wordList[Math.floor(Math.random() * wordList.length)].toUpperCase(); 
+//lägger valt ord i en array och splittrar ordet så det blir separata bokstäver.
+let arrSelectedWord = [];
+arrSelectedWord = selectedWord.split("");
+let letterBoxes = []; //Rutorna där bokstäverna ska stå
+//mitt html-element där bilden ligger - Bild som kommer vid fel svar
+let hangmanImg = document.getElementById("hangman");
+let hangmanImgNr = 0; //Börjar på 0 och räknas upp för varje fel bokstav // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
+let msgElem; 
+let startGameBtn; // Knappen du startar spelet med
+let lettersLeft = arrSelectedWord.length; // räknar ner hur många bokstäver som e kvar att gissa baserat på längden av slumpat ord
+let startTime; // Mäter tiden
 
-var wordList = ["Sol", "Träd", "Bänk", "Stol", "Knivig", "Busig", "Polis", "Gris", "Vegan", "Bil", "Rally", "Fralla", "Spring", "Regn"]; // Lista med spelets alla ord
-var selectedWord; // Ett av orden valt av en slumpgenerator
-var letterBoxes; //Rutorna där bokstäverna ska stå
-var hangmanImg; //Bild som kommer vid fel svar
-var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
-var msgElem; // Ger meddelande när spelet är över
-var startGameBtn; // Knappen du startar spelet med
-var letterButtons; // Knapparna för bokstäverna
-var startTime; // Mäter tiden
 
-// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
-// Initiering av globala variabler samt koppling av funktioner till knapparna.
-function init() {} // End init
 
-window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-/*funktion som just nu ändrar bakgrundsfärg när man trycker på starta spel-knappen men som ska ändras till att starta spelet vid tryck*/
-function startTheGame() {
-    document.querySelector("body").style.backgroundColor = "red";        
-}
-
-startGameBtn = document.getElementById("startGameBtn");
-startGameBtn.addEventListener('click', startTheGame); 
 
 // Funktion som slumpar fram ett ord
 
- /*Slumpar ord ur min ordlista, skriver ut dessa i meddelande-div:en sålänge.....*/
- for(var i=0;i<wordList.length;i++) {    
-    selectedWord = Math.floor(Math.random() * wordList.length);
-var randomWord = document.getElementById("message");
-     randomWord.textContent= wordList[selectedWord]; 
-} 
+// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
+// Initiering av globala variabler samt koppling av funktioner till knapparna.
+function init() {
+    console.log(init);
+} // End init
+
+window.onload = init; // Se till att init aktiveras då sidan är inladdad
+
+// När man klickar på knappen starta spel så anropas funktionen startTheGame
+startGameBtn = document.getElementById("startGameBtn");
+startGameBtn.addEventListener('click', startTheGame); 
+
+//funktionen startTheGame som i sin tur anropar funktionen ButtonValue och CreateInput.
+function startTheGame() {
+    event.target.disabled = true;
+    alpaButton = document.getElementById("letterButtons");
+    alpaButton.addEventListener('click', buttonValue);
+    createInput();
+    console.log(selectedWord);
+
+}
+
 
 // Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
-
+//skapa li-element där antalet rutor är beroende på längden av slumpat ord
+function createInput() { 
+   
+    for (let i = 0; i < arrSelectedWord.length; i++){
+        letterBoxes[i] = document.createElement("li");
+        letterBoxes[i].innerHTML = `<input type="text" disabled value="" class="list" />` 
+                    
+            document.getElementById("letterUL").appendChild(letterBoxes[i]);
+           
+            } 
+       
+        }
+    
+//vill ha tag i mina inputs för att ändra value till vald bokstav.....
+//ligger efter hela tiden och visar inte bokstav...... 
+let liInputElem = document.querySelectorAll('value');
 
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
-/*kod som just nu gör att en alert dyker upp när man klickar på bokstavsknapparna*/
-document.getElementById("letterButtons").addEventListener("click",function(){
-    alert("Du har klickat på en knapp!");});
+        // document.getElementById("letterButtons").addEventListener('click', function buttonValue() 
+        function buttonValue(){
+    //lägger value av vald knapp i letterButton och disable:ar vald boktav efter det.
+             letterButton = event.target.value;
+            event.target.disabled = true;
+         //om letterButton finns i arrayen med valda ordet = true
+                if(arrSelectedWord.indexOf(letterButton) !== -1) {
+                 //vill slå ihop min node-lista och min listarray...
+                    liInputElem = letterBoxes.join('');
+                    //var i ordet bokstaven finns där lägger den in bokstaven.
+              letterBoxes[arrSelectedWord.indexOf(letterButton)] = letterButton;
+             //om mina listelement när man slår ihop bokstäverna är det valda ordet så har personen vunnit!
+              if (letterBoxes.join('') == selectedWord) {
+                document.getElementById("message").textContent="Du har vunnit!" + " " + "ordet var: " + selectedWord;
+                //stäng av alla knapparna... typ remove eventhandler?
+              }     
+              //räknar ner antalet bokstäver kvar
+              lettersLeft--;
+                                       console.log(letterButton);
+                  } //finns bokstaven inte i arrayen med valda ordet = true
+                else if (arrSelectedWord.indexOf(letterButton) == -1)
+{
+   //räkna upp från 0
+    hangmanImgNr++
+    //ändra bild baserat på nummer av fel gissning. 
+    hangmanImg.src = "images/h"	+	hangmanImgNr	+	".png"	
+    //skriv ut fel bokstäver
+    document.getElementById("placeholders").innerHTML+=letterButton;
+    //om man har gissat fel 6 gånger skriv ett meddelande om att man har dött och vilket det rätta ordet var
+    if( hangmanImgNr == 6) {
+        document.getElementById("message").textContent="Du har dött!" + " " + "ordet var: " + selectedWord;
+    }
+    
+ 
+     console.log("finns inte!");
+}
+}
+  
+
+     
+    
+
+
+  
+
+
+
+
+
+
+
+
+
 
 
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
 
+
+
+
+
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+
+
+ 
+    
+    
+    
+
+
+
+
 
 
